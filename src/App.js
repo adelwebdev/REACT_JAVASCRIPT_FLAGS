fefd503d454031d4e9b3595875a797341c535fc2
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-18 19:41:34 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import About from "./pages/About";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/a-propos" exact component={About} />
-        <Route component={NotFound} />
+        <Route path="/" component={Home}></Route>
       </Switch>
     </BrowserRouter>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,17 @@
 import React from "react";
-import Logo from "../components/Logo";
-import Navigation from "../components/Navigation";
 
 const About = () => {
   return (
     <div>
-      <Navigation />
-      <Logo />
       <h1>A propos</h1>
       <br />
-      <p>lorem60</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi rerum,
+        alias cum voluptate temporibus hic maiores dolorem obcaecati corporis
+        dolorum.
+      </p>
     </div>
   );
 };
 
 export default About;
-<h1>A propos</h1>;
