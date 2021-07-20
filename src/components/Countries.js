d9ffc7de6bab4dc5fe4af6a3acdbdafd27a57385
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-20 21:15:35 +0300

    Add files via upload

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,14 +5,29 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [sotredData, setSortedData] = useState([]);
+  const [playOnce, SetPlayOnce] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
-      )
-      .then((res) => setData(res.data));
-  }, []);
+    if (playOnce) {
+      axios
+        .get(
+          "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+        )
+        .then((res) => {
+          setData(res.data);
+          SetPlayOnce(false);
+        });
+    }
+
+    const sortedCountry = () => {
+      const countryObj = Object.keys(data).map((i) => data[i]);
+      const sortedArray = countryObj.sort((a, b) => {
+        return b.population - a.population;
+      });
+      console.log(sortedArray);
+    };
+    sortedCountry();
+  }, [data]);
 
   return (
     <div className="countries">
