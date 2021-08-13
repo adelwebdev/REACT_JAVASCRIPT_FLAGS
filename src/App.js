cce63b97acbe2369754f7983544c4afd3855599b
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-13 23:05:06 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 // App: c le composant de base; on y insere toutes les pages
+
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 // un composant est une fonction
-
+// ceci est la procédure pour faire des pages web pr notre site
 const App = () => {
   return (
+    // ici on fait les liens url vers les pages
+    //pour faire des liens clickable, il faut créer un composant navigation (voir dans src)
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
