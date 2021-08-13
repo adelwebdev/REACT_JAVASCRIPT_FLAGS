34e1a30c91c6c761ccc54822fdf926e4f9caa65e
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-13 23:07:26 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,13 @@
-// const Home = () => {
-//   // apres return() c'est ce que le composant doit rendre!
-//   return (
-//     <div className="home">
-//       <h1>Accueil</h1>
-//     </div>
-//   );
-// };
-// export default Home;
+// apres return() c'est ce que le composant doit rendre!
 
 // avec rsc on import un statless component (ci-dessous)
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <div>
+      <Navigation />
       <h1>Accueil</h1>
     </div>
   );
