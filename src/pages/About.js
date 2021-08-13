47c304dbbbffa2e9e1c903018bd96bd396ea5f5b
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-13 23:07:41 +0300

    Add files via upload

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const About = () => {
   return (
     <div>
+      <Navigation />
       <h1>About</h1>
       <br />
       <p>
