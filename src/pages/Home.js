f12c593d4d667ca63994f98f90fff9cd51eb192d
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-14 02:27:39 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@
 
 // avec rsc on import un statless component (ci-dessous)
 import React from "react";
+import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <h1>Accueil</h1>
     </div>
