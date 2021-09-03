2f6726ea68cb9728d02db13a56033bdfacaaa30f
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-03 20:10:44 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import News from "./pages/News";
 import NotFound from "./pages/NotFound";
 
 // un composant est une fonction
@@ -11,11 +12,13 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     // ici on fait les liens url vers les pages
-    //pour faire des liens clickable, il faut créer un composant navigation (voir dans src)
+    // pour faire des liens clickable, il faut créer un composant navigation (voir dans src)
+    // il faut mettre les pages dans le bon ordre, jamais aprés {notFound}; sinon ça ne marche pas
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
+        <Route path="/news" exact component={News} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
