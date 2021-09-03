8edd5e5216ff00182cac911c60d7e678aae8ad3b
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-03 20:11:47 +0300

    Add files via upload

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  // activeClassName pr faire des liens clickable, il suit le "patch", exact to = "path"
   return (
     <div className="navigation">
       <NavLink exact to="/" activeClassName="nav-active">
@@ -9,6 +10,9 @@ const Navigation = () => {
       </NavLink>
       <NavLink exact to="/about" activeClassName="nav-active">
         About
+      </NavLink>
+      <NavLink exact to="/news" activeClassName="nav-active">
+        News
       </NavLink>
     </div>
   );
